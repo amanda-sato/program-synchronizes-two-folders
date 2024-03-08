575c338b91d058ec66512df42faa4ed40f89ffbc
Amanda Sato <amandareikosato@gmail.com>
2024-03-08 14:47:24 +0000

    test logging (basic configuration, send logging to file and show in console)

--- script-python.py
+++ script-python.py
@@ -11,18 +11,18 @@ def list_dir_contents(directory_path):
         )
 
 def copy_directory(source_path, destination_path):
-    source_folder = os.path.join(source_path, "challange-python")
+    source_folder = os.path.join(source_path, "folder-test")
     destination_folder = os.path.join(destination_path, "backup-challange")
 
     if os.path.exists(destination_folder):
         print("A pasta de destino já existe. Sincronizando...")
-        shutil.rmtree(destination_folder)  # Remove a pasta de destino e seu conteúdo
+        shutil.rmtree(destination_folder)  
 
     shutil.copytree(source_folder, destination_folder)
     print("Pasta copiada.")
 
 while True:
-    list_dir_contents("/Users/amand/challange-python")
-    copy_directory("/Users/amand", "/Users/amand")
+    list_dir_contents("/Users/amand/challange-python/program-synchronizes-two-folders/folder-test")
+    copy_directory("/Users/amand/challange-python/program-synchronizes-two-folders", "/Users/amand")
     time.sleep(30)
 
